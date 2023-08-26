0cb8d627e8d9a1b954f3b0a2f8a0da50a67e9c61
Yuzuki <50410851+Yuzuki616@users.noreply.github.com>
2023-08-26 13:51:35 +0800

    Update v2b.go

--- v2b/v2b.go
+++ v2b/v2b.go
@@ -49,7 +49,7 @@ type ServerInfo struct {
 	} `json:"networkSettings"`
 	CreatedAt     int         `json:"created_at"`
 	AllowInsecure int         `json:"insecure"`
-	LastCheckAt   string      `json:"last_check_at"`
+	LastCheckAt   interface{} `json:"last_check_at"`
 	Tags          interface{} `json:"tags"`
 	ServerName    string      `json:"server_name"`
 	ServerKey     string      `json:"server_key"`
