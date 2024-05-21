86ef09799d0839a85e51e61ffcb70bc113cf3a38
yuzuki999 <yuzuki2332@gmail.com>
2024-05-22 01:58:14 +0800

    fix mistake

--- cmd/uniproxy/main.go
+++ cmd/uniproxy/main.go
@@ -12,6 +12,7 @@ import (
 var host = flag.String("host", "127.0.0.1", "host")
 var port = flag.Int("port", 11451, "port")
 var config = flag.String("conf", "", "config file")
+var tun = flag.Bool("tun", false, "tun mode")
 
 func main() {
 	flag.Parse()
@@ -27,6 +28,7 @@ func main() {
 	case "error":
 		log.SetLevel(log.ErrorLevel)
 	}
+	proxy.TunMode = *tun
 	proxy.ResUrl = "http://127.0.0.1:" + strconv.Itoa(*port)
 	router.Init()
 	if err := router.Start(*host, *port); err != nil {
